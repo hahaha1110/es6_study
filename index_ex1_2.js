f8b47474641c516b29f63d33dc7a0a6407663858
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-04-08 22:03:33 +0900

    20230408

---
+++ index_ex1_2.js
@@ -0,0 +1,14 @@
+const number = 123n; 
+// console.log(typeof number);
+
+const name = 'hahaha';
+// console.log(typeof name);
+
+const age = 20;
+const job = '개발자';
+
+const msg = '저는 ' + job + '이고 ' + age + '살 입니다.';
+const msg2 = `저는 ${job}이고, ${age}살 입니다.`;
+
+console.log(msg);
+console.log(msg2);
