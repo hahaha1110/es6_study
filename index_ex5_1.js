741c4a462454a5a31c0499fb8382c67d763d4d80
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-04-11 23:41:32 +0900

    230411

---
+++ index_ex5_1.js
@@ -0,0 +1,3 @@
+const title = document.querySelector('h2'); // 태그 이름으로 선택
+title.innerText = '안녕하세요!';
+title.style.color = 'blue';
---
+++ index_ex5_2.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+
+function handler(event){
+    console.log('ha');
+}
+
+button.addEventListener('click', handler);
